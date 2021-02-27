83398e83809ea2f8f01b36da64603c2bc228a487
Isapna01 <isapna01@gmail.com>
2021-02-27 13:16:50 +0530

    added print comment

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
 //print the output
+//print
       System.out.println("Hello, World");
    }
 }
