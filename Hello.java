7150df9b17b596f8a624f01493ba61b3effc22b4
Isapna01 <isapna01@gmail.com>
2021-02-27 12:58:20 +0530

    This is my second commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
+//print the output
       System.out.println("Hello, World");
    }
 }
